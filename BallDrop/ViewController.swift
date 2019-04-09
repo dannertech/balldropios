ff17a438f3a22c3f3c3c2e9d41a9b0daf449e767
dannertech <danner@danner.tech>
2019-04-09 00:07:18 -0400

    Initial Commit

---
+++ BallDrop/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BallDrop
+//
+//  Created by Diamonique Danner on 4/9/19.
+//  Copyright © 2019 Danner Op., LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
